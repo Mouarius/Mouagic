478bfd721ae4831bf382f83c03670465cc06a671
mouarius <mouarius@gmail.com>
2014-03-30 22:09:59 +0200

    Adding References

--- main/java/fr/mouarius/mouagic/Mouagic.java
+++ main/java/fr/mouarius/mouagic/Mouagic.java
@@ -1,5 +1,65 @@
 package fr.mouarius.mouagic;
 
+/***
+ * Mouagic
+ * 
+ * Mouagic
+ * 
+ * 
+ * @author Mouarius
+ * @license GNU Lesser General Public License v2
+ *          (http://www.gnu.org/licenses/lgpl.html)
+ * @copyright Copyright (C) 2014  Mouarius
+ * 
+ *  
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ */
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import fr.mouarius.mouagic.lib.References;
+import fr.mouarius.mouagic.proxies.CommonProxy;
+
+
+@Mod(modid = References.MODID, name = References.MODNAME, version= References.MODVERSION)
 public class Mouagic {
+	
+@SidedProxy(clientSide = References.CLIENTPROXYLOCATION, serverSide = References.COMMONPROXYLOCATION)
+public static CommonProxy proxy;
+
+	@Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+		
+    }
+
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public static void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 
 }
---
+++ main/java/fr/mouarius/mouagic/lib/References.java
@@ -0,0 +1,21 @@
+package fr.mouarius.mouagic.lib;
+/***
+ * Mouagic
+ * 
+ * References
+ * 
+ * 
+ * @author Mouarius
+ * @license GNU Lesser General Public License v2
+ *          (http://www.gnu.org/licenses/lgpl.html)
+ * @copyright Copyright (C) 2014  Mouarius
+ * 
+ */
+public class References {
+	
+	public static final String MODID = "mouagic";
+	public static final String MODNAME = "Mouagic";
+	public static final String MODVERSION = "0.0.1";
+	public static final String CLIENTPROXYLOCATION = "fr.mouarius.mouagic.proxies" + ".ClientProxy";
+	public static final String COMMONPROXYLOCATION = "fr.mouarius.mouagic.proxies" + ".CommonProxy";
+}
